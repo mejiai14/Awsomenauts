4a8b761c4cc47c7a3ed8241e4628e1fe8effbdb5
ismael mejia <zombi3slaya@live.com>
2015-04-03 14:33:43 -0700

    9th

--- js/game.js
+++ js/game.js
@@ -28,7 +28,8 @@ var game = {
                 exp1: 0,
                 exp2: 0,
                 exp3: 0,
-                exp4: 0
+                exp4: 0,
+                win: ""
 	},
 	
 	
@@ -68,6 +69,7 @@ var game = {
                 me.pool.register("EnemyCreep", game.EnemyCreep, true);
                 me.pool.register("GameTimerManager", game.GameTimerManager);
                 me.pool.register("HeroDeathManager", game.HeroDeathManager);
+                me.pool.register("ExperienceManager", game.ExperienceManager);
             
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
