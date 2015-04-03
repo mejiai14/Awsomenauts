2d74cedae53224519e47ecb56778e4f64fa9dd5c
ismael mejia <zombi3slaya@live.com>
2015-04-03 14:53:18 -0700

    11th

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -55,14 +55,21 @@ game.ExperienceManager = Object.extend({
     
     update: function(){
         if(game.data.win === true && !this.gameOver){
-            game.data.exp += 10;
-            this.gameOver = true;
+            this.gameOver(true);
         }else if(game.data.win === false && !this.gameOver){
-            game.data.exp += 1;
-            this.gameOver = true;
+            this.gameOver(false);
         }
-        console.log(game.data.exp);
         
         return true;
+    },
+    
+    gameOver: function(win){
+        if(win){
+            game.data.exp += 10;
+        }else{
+            game.data.exp += 1;
+        }
+        this.gameOver = true;
+        me.save.exp = game.data.exp;
     }
 });
