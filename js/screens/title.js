f94fe42e4bb7bd24d81b765807f3f2f42af8727d
ismael mejia <zombi3slaya@live.com>
2015-04-06 15:01:55 -0700

    12th

--- js/screens/title.js
+++ js/screens/title.js
@@ -30,6 +30,27 @@ game.TitleScreen = me.ScreenObject.extend({
                         me.state.change(me.state.PLAY);
                     }
                 })));
+                
+                me.game.world.addChild(new (me.Renderable.extend({
+                    init: function(){
+                        this._super(me.Renderable, 'init', [380, 340, 250, 50]);
+                        this.font = new me.Font("Arial", 46, "white");
+                        me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
+                    },
+                    
+                    draw: function(renderer){
+                        this.font.draw(renderer.getContext(), "Continue", this.pos.x, this.pos.y);
+                    },
+                    
+                    update: function(dt){
+                        return true;
+                    },
+                    
+                    newGame: function(){
+                        me.input.releasePointerEvent('pointerdown', this);
+                        me.state.change(me.state.PLAY);
+                    }
+                })));
 	},
 	
 	
